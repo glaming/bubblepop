4320d3da892d46f753dc90e080e2f530c5fd165f
Graham Laming <graham@laming.me>
2017-07-09 09:01:45 +0100

    Added Typescript, webpack. Using local Phaser

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var path = require("path");
+
+// Phaser webpack config
+var phaserModule = path.join(__dirname, '/node_modules/phaser-ce/');
+var phaser = path.join(phaserModule, 'build/custom/phaser-split.js');
+var pixi = path.join(phaserModule, 'build/custom/pixi.js');
+var p2 = path.join(phaserModule, 'build/custom/p2.js');
+
+module.exports = {
+    entry: {
+        app: "./src/app.ts"
+    },
+    output: {
+        filename: "bundle.js"
+    },
+    resolve: {
+        alias: {
+            'phaser': phaser,
+            'pixi': pixi,
+            'p2': p2
+        }
+    },
+    module: {
+        loaders: [
+            { test: /\.tsx?$/, loader: "ts-loader", exclude: /node_modules/ },
+            { test: /pixi\.js/, use: ['expose-loader?PIXI'] },
+            { test: /phaser-split\.js$/, use: ['expose-loader?Phaser'] },
+            { test: /p2\.js/, use: ['expose-loader?p2'] }
+        ]
+    }
+};
